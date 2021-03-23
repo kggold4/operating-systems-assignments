ff83a5b040801347a36fece7c87845c976f6c193
kfir <kggold4@gmail.com>
2021-03-23 21:27:04 +0200

    uplode 3 txt file of output from main2_* and done main2_4

--- task_b/main2_4.c
+++ task_b/main2_4.c
@@ -3,32 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
+// defines
+#define SLEEP 20
 
 // main function
 int main() {
 
-    
+    // run main2_1, main2_2 and main2_3 files
+    system("./main2_1\n");
+    system("./main2_2\n");
+    system("./main2_3\n");
+    sleep(SLEEP);
 
-    execv("./main2_1 & ./main2_2 & ./main2_3", NULL);
-    printf("run\n");
+    // grep daemon from main2_3
+    system("pstree | grep myDaemon /var/log/syslog");
 
-    // char *args1[] = {"./main2_1", NULL};
-    // char *args2[] = {"./main2_2", NULL};
-    // char *args3[] = {"./main2_3", NULL};
-    // char **args[] = {args1, args2, args3};
-
-    // int i = 0;
-    // while(i < 3) {
-
-    //     __pid_t pid = fork();
-
-    //     if(pid != 0) {
-    //         printf("./main2_%d\n", i + 1);
-    //         execv(args[i][0], args[i]);
-    //     }
-    //     i++;
-    // }
-    
     return 0;
 }
