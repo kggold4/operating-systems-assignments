4faca087e34dc405f58d67922e5dd06bc37e1419
kfir <kggold4@gmail.com>
2021-03-23 18:55:46 +0200

    started write main2_4.c

--- task_b/main2_4.c
+++ task_b/main2_4.c
@@ -1,5 +1,31 @@
+// includes
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+
+
+// main function
 int main() {
+
+    printf("./main2_1\n");
+
+    char *args1[] = {"./main2_1", NULL};
+    char *args2[] = {"./main2_2", NULL};
+    char *args3[] = {"./main2_3", NULL};
+    char **args[] = {args1, args2, args3};
+
+    int i = 0;
+    while(i < 3) {
+
+        __pid_t pid = fork();
+
+        if(pid != 0) {
+            printf("./main2_%d\n", i + 1);
+            execv(args[i][0], args[i]);
+        }
+        i++;
+    }
+    
     return 0;
 }
